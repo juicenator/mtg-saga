3aea015b2893f30721a38e79e102f86a4cbeec8b
Juicenator <joosvanzweeden@live.nl>
2023-05-13 16:15:55 +0200

    error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { CssBaseline, AppBar, Toolbar, Button, Link, Typography } from '@mui/material';
+
+// Pages
 import DeckInfo from './DeckInfo';
+import ErrorPage from './ErrorPage';
+
+// Styles and meterial
 import useStyles from './Styles';
 import './App.css';
+import { CssBaseline, AppBar, Toolbar, Button, Link, Typography } from '@mui/material';
 
 import {
   createBrowserRouter,
@@ -14,6 +19,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <DeckInfo />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
